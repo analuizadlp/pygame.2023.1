3cc2a7a002fcfd0bf228cb12b08497b17d9fb725
Ana Luiza de la Pena <analblp@al.insper.edu.br>
2023-05-24 09:53:10 -0300

    adicionando a musica nos arquivos e no codigo

--- tetris.py
+++ tetris.py
@@ -7,10 +7,14 @@ from functions import tela_inicio , tela_final, tela_jogo
 img_fundo_jogo=pygame.image.load('images/img.png')
 img_fundo_jogo = pygame.transform.scale(img_fundo_jogo, (tela ))
 pygame.init()
+pygame.mixer.init()
 fonte=pygame.font.Font('fonte/PressStart2P.ttf', 28)
 screen=pygame.display.set_mode(tela)
 timer=pygame.time.Clock()
 state= start
+pygame.mixer.music.load('sons/musica.wav')
+pygame.mixer.music.set_volume(0.4)
+pygame.mixer.music.play(loops=-1)
 while state!= done:
     print(state)
     if state == start:
